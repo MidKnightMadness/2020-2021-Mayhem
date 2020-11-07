8443f19a7547c0b3f5bc7c5f50f5a3daeb1f88e9
Ken Mori <ken.mori@students.tka.org>
2020-11-07 11:19:14 -0800

    Made changes to teamcode, added new subassemblies for code. Also updated concept vuforia/tensorflow and added it to our visual.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomousplan.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+public class autonomousplan {
+
+
+    //Plan A
+
+    //1. go to midline
+    //2. shoot power shot
+    //3. go to designated square and put wobble goal there
+    //4. go drop rings
+    //5. parking on the midline
+
+
+    //Plan B
+
+    //1. go to midline
+    //2. go drop rings
+    //3. go to designated square and put wobble goal there
+    //4. parking on midline
+    //5. shoot power shot
+
+
+}
